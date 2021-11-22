4eb98cf93e45ee05b0359b1cdce358fc64bcf773
“maeuza” <maeuza@hotmail.com>
2021-11-22 16:09:44 -0500

    hacer conexiones

--- app/src/main/java/com/TSRosies/naturaltravel/detalle/DetalleActivity.kt
+++ app/src/main/java/com/TSRosies/naturaltravel/detalle/DetalleActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import com.TSRosies.naturaltravel.R
 import com.TSRosies.naturaltravel.databinding.ActivityDetalleBinding
+import com.TSRosies.naturaltravel.model.SitioNaturalItem
+import com.squareup.picasso.Picasso
 import org.imaginativeworld.whynotimagecarousel.ImageCarousel
 import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel.Companion as ImageCarousel1
 
 class DetalleActivity : AppCompatActivity() {
 
@@ -18,13 +21,25 @@ class DetalleActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         detalleBinding = ActivityDetalleBinding.inflate(layoutInflater)
+        setContentView(detalleBinding.root)
         /* setContentView(R.layout.activity_detalle) */
+
         val carousel: ImageCarousel = findViewById(R.id.imagen_carousel)
         list.add(CarouselItem(imageDrawable = R.drawable.laguna_de_pedro_palo))
-        list.add(CarouselItem(imageDrawable = R.drawable.laguna_de_pedro_palo2))
+        list.add(CarouselItem(imageDrawable = R.drawable.laguna_de_pedro_palo2 ))
         carousel.addData(list)
 
-        val nombre = intent.extras?.getString("nombre")
-        detalleBinding.nombreTextView.text =nombre
-    }
+        val sitionatural: SitioNaturalItem = intent.extras?.getSerializable("SitioNaturalItem ") as SitioNaturalItem
+
+        with(detalleBinding) {
+            nombreTextView.text = sitionatural.nombre
+            descripcion2TextView.text = sitionatural.descripcion2
+            ubicacionTextView.text = sitionatural.ubicacion
+            temperaturaTextView.text = sitionatural.temperatura
+            actividadesTextView.text = sitionatural.actividades
+         //   Picasso.get().load(sitionatural.urlImagen1).into(carousel)
+
+        }
+
+        }
 }
