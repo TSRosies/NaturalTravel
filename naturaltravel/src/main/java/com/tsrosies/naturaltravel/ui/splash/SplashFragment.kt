814aa4b17cbbbf2929de3e91f0ae567e56e6df20
milecafa <milena.cafa@gmail.com>
2021-12-10 09:30:29 -0500

    Json actualizado y splash funcional

--- naturaltravel/src/main/java/com/tsrosies/naturaltravel/ui/splash/SplashFragment.kt
+++ naturaltravel/src/main/java/com/tsrosies/naturaltravel/ui/splash/SplashFragment.kt
@@ -24,7 +24,7 @@ class SplashFragment : Fragment() {
         Handler(Looper.myLooper()!!).postDelayed({
             findNavController().navigate(R.id.action_splashFragment_to_navigation_list)
 
-        }, 2000)
+        }, 6000)
 
         return view
     }
